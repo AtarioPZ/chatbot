4361ee068553a5a04e421345df649e8dfbd7930c
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2023-05-29 21:36:35 +0530

    Initial nodes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import React, { useCallback } from 'react';
-import ReactFlow, {
-  MiniMap,
-  Controls,
-  Background,
-  useNodesState,
-  useEdgesState,
-  addEdge,
-} from 'reactflow';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useCallback,  useState } from 'react'
+import ReactFlow, { useNodesState, useEdgesState, addEdge } from 'reactflow';
+import './App.css'
 import 'reactflow/dist/style.css';
 
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  { id: '1', position: { x: 0, y: 0 }, data: { label: 'Text Message 1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: 'Text Message 2' } },
 ];
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
@@ -23,18 +17,17 @@ export default function App() {
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{width: '100%', height: '100vh'}}>
+      <h1>Chatbot Flow Builder</h1>
       <ReactFlow
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
-      >
-        <Controls />
-        <MiniMap />
-        <Background variant="dots" gap={12} size={1} />
+      >        
       </ReactFlow>
+      <h1>END</h1>
     </div>
   );
 }
